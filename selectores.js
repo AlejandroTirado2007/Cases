ee64ed2170f594285c7fd27f67ae3717e701c3fa
alejandrotirado2007 <a>
2022-03-30 15:56:23 -0500

    a

---
+++ selectores.js
@@ -0,0 +1,45 @@
+let boton = document.getElementById("boton");
+boton.addEventListener("click", function (event) {
+
+    let selector = document.getElementById("selector").value
+
+
+    switch(selector){
+        case "1" :
+            alert("Elegiste 1")
+            break
+        case "2" :
+            alert("Elegiste 2")
+            break
+        case "3" :
+            alert("Elegiste 3")
+            break
+        case "4" :
+            alert("Elegiste 4")
+            break
+        case "5" :
+            alert("Elegiste 5")
+            break
+        case "6" :
+            alert("Elegiste 6")
+            break
+        case "7" :
+            alert("Elegiste 7")
+            break
+        case "8" :
+            alert("Elegiste 8")
+            break
+        case "9" :
+            alert("Elegiste 9")
+            break
+        case "10" :
+            alert("Elegiste 10")
+            break
+        default: 
+            alert("Opción no valida")
+    }
+
+    
+    
+})
+
